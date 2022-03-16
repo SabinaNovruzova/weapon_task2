34e0594506656370aff6493afc33dd8336eb5e08
Sabina Novruzova <tu7cldm19@aztu.local>
2022-03-16 16:23:58 +0400

    commit

---
+++ classtask/Model/Weapon.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace classtask.Model
+{
+    class Weapon
+    {
+        public int bulletCapacity { get; set; }
+        public int countBullet { get; set; }
+        public int secondsOfCombDischarge { get; set; }
+        public int shootingMode { get; set; }
+        
+
+        public Weapon(int bulletCapacity,int countBullet,int secondsOfCombDischarge, int shootingMode)
+        {
+            this.bulletCapacity = bulletCapacity;
+            this.countBullet = countBullet;
+            this.secondsOfCombDischarge = secondsOfCombDischarge;
+            this.shootingMode = shootingMode;
+        }
+        public void Shoot()
+        {
+            int a = countBullet;
+            if (bulletCapacity>0 || countBullet>0)
+            {
+                a -= 1;
+                Console.WriteLine("BUM! ates acildi");
+            }
+            else
+            {
+                Console.WriteLine("atmaq ucun gulleniz yoxdur");
+            } 
+        }
+        public void Fire()
+        {
+            int secondShootEnd = (countBullet * secondsOfCombDischarge) / bulletCapacity;
+            Console.WriteLine(secondShootEnd);
+        }
+        public int GetRemainBulletCount()
+        {
+            int numberOfBulletsNeeded = bulletCapacity - countBullet;
+            return numberOfBulletsNeeded;
+        }
+       public void Reload()
+        {
+            countBullet = bulletCapacity;
+        }
+        public void ChangeFireMode()
+        {
+            if (shootingMode==3)
+            {
+                shootingMode = 4;
+                Console.WriteLine("avtomatikdir");
+            }
+            else if (shootingMode==4)
+            {
+                shootingMode = 3;
+                Console.WriteLine("teklidir");
+            }
+        }
+        public void Editing()
+        {
+            Console.WriteLine("tutumu redakte etmek ucun yeni qiymet verin:");
+            bulletCapacity = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("gulle sayini deyismek ucun yeni qiymet verin:");
+            countBullet = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("daragin bosalma saniyesini deyismek ucun yeni qiymet yazin:");
+            secondsOfCombDischarge = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+    
+}
---
+++ classtask/Program.cs
@@ -0,0 +1,68 @@
+﻿using classtask.Model;
+using System;
+
+namespace classtask
+{   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Weapon wep = new Weapon(40, 30, 5, 4);
+           
+                int key = 0;
+            do
+            {
+                Console.WriteLine("Reqem daxil edin : ");
+                key = Convert.ToInt32(Console.ReadLine());
+                switch (key)
+                {
+                    case 1:
+                        Console.WriteLine("1ci metod");
+                        wep.Shoot();
+                        break;
+                    case 2:
+                        Console.WriteLine("2ci metod");
+                         wep.Fire();
+                        break;
+                    case 3:
+                        Console.WriteLine("3cu metod");
+                        if (wep.GetRemainBulletCount()==0)
+                        {
+                            Console.WriteLine("daraq doludur");                              
+                        }
+                        else if (wep.GetRemainBulletCount()>0)
+                        {
+                            Console.WriteLine($"{"daragin dolmasi ucun"} {wep.GetRemainBulletCount()} {"gulle lazimdir"}");
+                        }
+                        break;
+                    case 4:
+                        Console.WriteLine("4cu metod");
+                        wep.Reload();
+                        Console.WriteLine("daraq doldu");
+                        break;
+                    case 5:
+                        Console.WriteLine("5ci  metod");
+                        wep.ChangeFireMode();
+                        break;
+                    case 6:
+                        Console.WriteLine("Sagolun");
+                        break;
+                    case 7:
+                        Console.WriteLine("7ci metod");
+                        wep.Editing();
+                        break;
+                    case 0:
+                        Console.WriteLine("0ci  metod");
+                        Console.WriteLine($"{"Daragin tutumu:"} {wep.bulletCapacity}");
+                        Console.WriteLine($"{"Daraqdaki gulle sayi:"} {wep.countBullet}");
+                        Console.WriteLine($"{"Daragin bosalma saniyesi:"} {wep.secondsOfCombDischarge}");
+                        Console.WriteLine($"{"Atis modu:"} {wep.shootingMode}");
+                        break;
+                    default:
+                        Console.WriteLine("Sehv daxil etdiniz");
+                        break;
+                }
+            } while (key != 6);
+        }
+    }
+}
